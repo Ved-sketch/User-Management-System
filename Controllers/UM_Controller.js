c4afebb94bf29649586d196da28377ef8a15476f
Ved-sketch <vedgupta1806@gmail.com>
2026-01-19 09:41:30 +0530

    add the getUserById function.

--- Controllers/UM_Controller.js
+++ Controllers/UM_Controller.js
@@ -41,3 +41,15 @@ export async function updateUser(req,res){
         res.status(500).json({message: "Internal server error"});
     }
 }
+
+export async function getUserById(req,res){
+    try{
+        const user = await User.findById(req.params.id);
+        if(!user) res.status(404).json({message:"User not found"});
+        res.status(200).json(user);
+    }
+    catch(error){
+        console.error("Error in getUserById function",error);
+        res.status(500).json({message:"Internal server error"})
+    }
+}
--- Routes/infoRoutes.js
+++ Routes/infoRoutes.js
@@ -1,10 +1,11 @@
 import express from "express"
-import {createUser, getAllUser, updateUser} from "../Controllers/UM_Controller.js"
+import {createUser, getAllUser, updateUser, getUserById} from "../Controllers/UM_Controller.js"
 
 const router = express.Router();
 
 router.get("/",getAllUser);
 router.post("/",createUser);
 router.put("/:id",updateUser);
+router.get("/:id",getUserById);
 
 export default router;
